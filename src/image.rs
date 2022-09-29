2b28a0113180b0180946daf30b047771274e64e0
Cas Brugman <casbrugman@hotmail.nl>
2022-09-29 18:11:53 +0200

    Added get_pixel method

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{convert::TryFrom, path::Path};
 
 use bevy::{
     prelude::{Image as BevyImage, *},
@@ -6,30 +6,46 @@ use bevy::{
 };
 use image::{DynamicImage, ImageBuffer};
 
+#[derive(Deref, DerefMut)]
 pub struct ImageHelper<'a> {
     image: &'a mut BevyImage,
 }
+
+const OFFSET: u32 = 4;
 
 impl<'a> ImageHelper<'a> {
     pub fn new(image: &'a mut BevyImage) -> Self {
         Self { image }
     }
 
-    pub fn set_pixel(&mut self, position: Vec2, color: Color) -> Result<(), &'static str> {
-        let size = self.image.size();
+    fn get_index(&mut self, position: Vec2) -> Result<usize, &'static str> {
+        let size = self.image.size().as_uvec2();
 
-        if position.x < 0. || position.x > size.x || position.y < 0. || position.y > size.y {
+        let position = if position.x.is_sign_positive() && position.y.is_sign_positive() {
+            position.as_uvec2()
+        } else {
+            return Err("negative position");
+        };
+
+        if position.x >= size.x || position.y >= size.y {
             return Err("position is outside image");
         }
 
-        let (size, position) = (size.as_uvec2(), position.as_uvec2());
+        let i = ((size.x * position.y + position.x) * OFFSET) as usize;
 
+        if i >= self.image.data.len() {
+            return Err("position is outside image");
+        }
+
+        Ok(i)
+    }
+
+    pub fn set_pixel(&mut self, position: Vec2, color: Color) -> Result<(), &'static str> {
         match self.image.texture_descriptor.format {
             TextureFormat::Rgba8UnormSrgb => {
-                let offset = 4;
-                let i = ((size.x * (position.y) + position.x) * offset) as usize;
+                let i = self.get_index(position)?;
                 self.image.data.splice(
-                    i..(i + offset as usize),
+                    i..(i + OFFSET as usize),
                     color.as_linear_rgba_u32().to_le_bytes(),
                 );
                 Ok(())
@@ -38,10 +54,25 @@ impl<'a> ImageHelper<'a> {
         }
     }
 
-    pub fn save(&self, path: impl AsRef<Path>) -> Result<(), ()> {
-        let image = DynamicImage::try_from(self.to_owned())?;
+    pub fn get_pixel(&mut self, position: Vec2) -> Result<Color, &'static str> {
+        let bytes: [u8; 4] = match self.image.texture_descriptor.format {
+            TextureFormat::Rgba8UnormSrgb => {
+                let i = self.get_index(position)?;
+                self.image.data[i..(i + OFFSET as usize)]
+                    .try_into()
+                    .map_err(|_| "could not convert slice to array")?
+            }
+            _ => return Err("textureformat not supported"),
+        };
 
-        image.save(path).map_err(|_| ())
+        Ok(Color::rgba_u8(bytes[0], bytes[1], bytes[2], bytes[3]))
+    }
+
+    pub fn save(&self, path: impl AsRef<Path>) -> Result<(), &'static str> {
+        let image =
+            DynamicImage::try_from(self.to_owned()).map_err(|_| "could not convert image")?;
+
+        image.save(path).map_err(|_| "could not save image")
     }
 }
 
