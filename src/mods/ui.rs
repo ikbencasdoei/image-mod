9fff8bf0b441afa60607ca520cfa94ff9f42d2b7
Cas Brugman <casbrugman@hotmail.nl>
2022-12-17 18:12:48 +0100

    Structure ui

--- src/mods/ui.rs
+++ src/mods/ui.rs
@@ -2,10 +2,11 @@ use std::any::TypeId;
 
 use bevy::prelude::*;
 use bevy_egui::{
-    egui::{self, CollapsingHeader, Id},
+    egui::{self, CollapsingHeader, Id, Ui},
     EguiContext,
 };
 use dyn_clone::DynClone;
+use uuid::Uuid;
 
 use crate::editor::Editor;
 
@@ -70,82 +71,96 @@ fn add_ui(
     });
 }
 
+fn show_selections(ui: &mut Ui, modification: &mut Modification) {
+    CollapsingHeader::new(format!(
+        "selections ({})",
+        modification.get_selection().len()
+    ))
+    .default_open(true)
+    .enabled(!modification.get_selection().is_empty())
+    .show(ui, |ui| {
+        let mut remove_selection = None;
+        for (index, selection) in modification.get_selection().iter().enumerate() {
+            ui.label(selection.index.name.as_str());
+            ui.menu_button("remove", |ui| {
+                if ui.button("sure?").clicked() {
+                    remove_selection = Some(index);
+                    ui.close_menu();
+                }
+            });
+        }
+
+        if let Some(index) = remove_selection {
+            modification.remove_selection(index);
+        }
+    });
+}
+
+fn show_modifier(
+    ui: &mut Ui,
+    modification: &mut Modification,
+    index: usize,
+    selected: &mut Option<Uuid>,
+) -> bool {
+    let mut remove = false;
+    let id = ui.make_persistent_id(modification.id);
+    egui::collapsing_header::CollapsingState::load_with_default_open(ui.ctx(), id, true)
+        .show_header(ui, |ui| {
+            ui.label(format!("#{index}"));
+            if ui
+                .toggle_value(
+                    &mut (*selected == Some(modification.id)),
+                    modification.index.name.as_str(),
+                )
+                .clicked()
+            {
+                *selected = Some(modification.id);
+            }
+            ui.menu_button("remove", |ui| {
+                if ui.button("sure?").clicked() {
+                    remove = true;
+                    ui.close_menu();
+                }
+            });
+        })
+        .body(|ui| {
+            if modification.cache.is_some() {
+                ui.label("cached");
+            }
+
+            modification.modifier.view(ui);
+
+            show_selections(ui, modification);
+        });
+
+    remove
+}
+
+fn show_mods(ui: &mut Ui, editor: &mut Editor) {
+    if editor.get_mods().is_empty() {
+        ui.label("(empty)");
+    } else {
+        let mut remove_mod = None;
+        let mut selected_mod = editor.get_selected_mod();
+        for (i, modification) in editor.iter_mut_mods().enumerate().rev() {
+            if show_modifier(ui, modification, i, &mut selected_mod) {
+                remove_mod = Some(modification.id);
+            }
+        }
+
+        if let Some(index) = remove_mod {
+            editor.remove_mod(index);
+        }
+
+        if let Some(id) = selected_mod {
+            editor.select_mod(id).ok();
+        }
+    }
+}
+
 fn edit_ui(mut egui_context: ResMut<EguiContext>, mut editor: ResMut<Editor>) {
     let name = "Modifiers";
     egui::Window::new(format!("{name} ({})", editor.get_mods().len()))
         .id(Id::new(name))
-        .show(egui_context.ctx_mut(), |ui| {
-            if editor.get_mods().is_empty() {
-                ui.label("(empty)");
-            } else {
-                let mut remove_mod = None;
-                let mut selected_mod = editor.get_selected_mod();
-                for (i, modification) in editor.iter_mut_mods().enumerate().rev() {
-                    let id = ui.make_persistent_id(modification.id);
-                    egui::collapsing_header::CollapsingState::load_with_default_open(
-                        ui.ctx(),
-                        id,
-                        true,
-                    )
-                    .show_header(ui, |ui| {
-                        ui.label(format!("#{i}"));
-                        if ui
-                            .toggle_value(
-                                &mut (selected_mod == Some(modification.id)),
-                                modification.index.name.as_str(),
-                            )
-                            .clicked()
-                        {
-                            selected_mod = Some(modification.id);
-                        }
-                        ui.menu_button("remove", |ui| {
-                            if ui.button("sure?").clicked() {
-                                remove_mod = Some(modification.id);
-                                ui.close_menu();
-                            }
-                        });
-                    })
-                    .body(|ui| {
-                        if modification.cache.is_some() {
-                            ui.label("cached");
-                        }
-
-                        modification.modifier.view(ui);
-
-                        CollapsingHeader::new(format!(
-                            "selections ({})",
-                            modification.get_selection().len()
-                        ))
-                        .default_open(true)
-                        .enabled(!modification.get_selection().is_empty())
-                        .show(ui, |ui| {
-                            let mut remove_selection = None;
-                            for (index, selection) in
-                                modification.get_selection().iter().enumerate()
-                            {
-                                ui.label(selection.index.name.as_str());
-                                ui.menu_button("remove", |ui| {
-                                    if ui.button("sure?").clicked() {
-                                        remove_selection = Some(index);
-                                        ui.close_menu();
-                                    }
-                                });
-                            }
-
-                            if let Some(index) = remove_selection {
-                                modification.remove_selection(index);
-                            }
-                        });
-                    });
-                }
-
-                if let Some(index) = remove_mod {
-                    editor.remove_mod(index);
-                }
-
-                if let Some(id) = selected_mod {
-                    editor.select_mod(id).ok();
-                }
-            }
-        });
+        .show(egui_context.ctx_mut(), |ui| show_mods(ui, &mut *editor));
 }
