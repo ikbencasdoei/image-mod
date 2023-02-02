170c8e3edf30b2a01114a39fa53cbebaeb68104f
Cas Brugman <casbrugman@hotmail.nl>
2023-02-02 01:52:13 +0100

    Add file picker to Source

--- src/modifier/collection/source.rs
+++ src/modifier/collection/source.rs
@@ -1,26 +1,45 @@
 use std::{
     path::{Path, PathBuf},
     str::FromStr,
+    sync::mpsc::Receiver,
 };
 
 use egui::{TextEdit, Ui};
 
 use crate::{
     editor::Editor,
+    file_picker::PickerResult,
     image::Image,
     modifier::{modification::Output, traits::Modifier},
 };
 
-#[derive(Clone, Default, PartialEq)]
+#[derive(Default)]
 pub struct Source {
     pub path: PathBuf,
+    receiver: Option<Receiver<Option<PickerResult>>>,
 }
 
 impl Source {
     pub fn new(path: impl AsRef<Path>) -> Self {
         Self {
             path: path.as_ref().to_path_buf(),
+            ..Default::default()
         }
+    }
+}
+
+impl Clone for Source {
+    fn clone(&self) -> Self {
+        Self {
+            path: self.path.clone(),
+            receiver: None,
+        }
+    }
+}
+
+impl PartialEq for Source {
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
     }
 }
 
@@ -29,13 +48,24 @@ impl Modifier for Source {
         Image::open(&self.path).ok()
     }
 
-    fn view(&mut self, ui: &mut Ui, _: &mut Editor) {
+    fn view(&mut self, ui: &mut Ui, editor: &mut Editor) {
+        if let Some(receiver) = &self.receiver {
+            if let Ok(Some(PickerResult::PickedLoad(result))) = receiver.try_recv() {
+                self.path = result;
+            }
+        }
+
         ui.horizontal(|ui| {
             ui.label("path:");
             let mut string = self.path.to_string_lossy().to_string();
-            let response = ui.add(TextEdit::singleline(&mut string));
-            if response.changed() {
+            if ui.add(TextEdit::singleline(&mut string)).changed() {
                 self.path = PathBuf::from_str(&string).unwrap();
+            }
+        });
+
+        ui.add_enabled_ui(!editor.picker.is_open(), |ui| {
+            if ui.button("open file picker").clicked() {
+                self.receiver = editor.picker.picker_with_channel().ok();
             }
         });
     }
